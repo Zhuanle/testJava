9ccfd00bc2da095d6c034a63abbc5749da0d3a59
Zhuanle <1847496393@qq.com>
2019-12-16 14:10:07 +0800

    testJava2

--- src/com/zhl/Main.java
+++ src/com/zhl/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("JavaTest");
+        for (int i=1;i < 5;i++){
+            System.out.println(i);
+        }
+
     }
 }
